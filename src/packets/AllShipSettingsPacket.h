b7cd894575d5c7e27d8bc03326a7314f579cdc74
Tsubashi <8th.realm@gmail.com>
2015-06-20 13:20:49 -0600

    Restored accidentally deleted files (Whoops...)

---
+++ src/packets/AllShipSettingsPacket.h
@@ -0,0 +1,29 @@
+#pragma once
+//------------------------------------------------------------------------------
+// FILE:      AllShipSettingsPacket.h
+// PURPOSE:   Provide a tcp client object
+// COPYRIGHT: Copyright (c) 2015 Tsubashi
+//           
+// Distributed under The MIT License (MIT)
+//
+//------------------------------------------------------------------------------
+
+//----- Included files ---------------------------------------------------------
+// 1. Standard library headers
+// 2. External library headers
+// 3. Shared code headers
+// 4. Non-shared code headers
+//----- Defines ----------------------------------------------------------------
+//----- Forward declarations ---------------------------------------------------
+//----- Namespace declaration --------------------------------------------------
+namespace packets {
+//----- Constants / Enumerations -----------------------------------------------
+//----- Structs / Classes ------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////
+/// \class serialNMEA
+///
+/// This class is used to open a serial port, read lines, and check to make sure
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace
